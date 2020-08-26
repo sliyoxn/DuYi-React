bfa13fc6e1e1b1b74c47684f7f63fa7acbc58bcf
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2020-08-26 10:14:03 +0800

    backups

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -7,13 +7,13 @@ import rootSaga from "./saga"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { routerMiddleware } from "connected-react-router"
 import history from "./history"
-const routerMid = routerMiddleware(history)
+const routerMid = routerMiddleware(history);
 
 const sagaMid = createSagaMiddleware(); //创建一个saga的中间件
 
 const store = createStore(reducer,
     composeWithDevTools(applyMiddleware(routerMid, sagaMid, logger))
-)
+);
 
 sagaMid.run(rootSaga); //启动saga任务
 
