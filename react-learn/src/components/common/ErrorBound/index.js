096e207de647f6dbb810b4a668b5961f362e3492
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-02-01 20:22:43 +0800

    deo

--- react-learn/src/components/common/ErrorBound/index.js
+++ react-learn/src/components/common/ErrorBound/index.js
@@ -6,6 +6,7 @@ export default class ErrorBound extends PureComponent {
         hasError: false
     }
 
+    // 处理错误
     static getDerivedStateFromError(error) {
         console.log("发生错误了");
         return {
@@ -13,18 +14,14 @@ export default class ErrorBound extends PureComponent {
         }
     }
 
+    // 记录错误
     componentDidCatch(error, info) {
         console.log("记录错误信息");
     }
 
-
-
     render() {
-        // setTimeout(() => {
-        //     throw new Error("asfasdfasf");
-        // }, 1000);
         if (this.state.hasError) {
-            return <h1>出现错误了！</h1>
+            return <h1>出现错误了</h1>
         }
         return this.props.children
     }
