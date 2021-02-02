298513599d4a2029974f2518ce82e12101a6b555
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-02-02 21:40:17 +0800

    demo

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,9 +1,7 @@
 import React, { useState, useRef } from 'react'
-window.arr = [];
 
 export default function App() {
     const inpRef = useRef();
-    window.arr.push(inpRef);
     const [n, setN] = useState(0)
     return (
         <div>
@@ -11,7 +9,6 @@ export default function App() {
             <button onClick={() => {
                 console.log(inpRef.current.value)
             }}>得到input的值</button>
-
             <input type="number"
                 value={n}
                 onChange={e => {
