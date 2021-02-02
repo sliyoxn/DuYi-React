12c54ebd3b06104d4321bca4a2eee9ea6e4ef222
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-02-02 14:11:48 +0800

    rua

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import useAllStudents from "./myHooks/useAllStudents"
 
+import { useEffect, useState } from "react"
+import { getAllStudents } from "../services/student"
+
+/**
+ * 当组件首次加载完成后，获取所有学生数据
+ */
+export default function useAllStudents() {
+    const [students, setStudents] = useState([])
+    useEffect(() => {
+        (async ()=>{
+            const stus = await getAllStudents();
+            setStudents(stus);
+        })();
+    }, [])
+    return students;
+}
+
 function Test() {
     const stus = useAllStudents();
     const list = stus.map(it => <li key={it.id}>{it.name}</li>)
